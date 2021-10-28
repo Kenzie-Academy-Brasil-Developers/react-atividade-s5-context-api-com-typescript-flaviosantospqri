7a79e43c20fbddfb0840bff78ff8e8b3c6a0fb22
Flávio Santos <flavioraph@live.com>
2021-10-27 23:43:25 -0300

    Versao de Inicializaçao

---
+++ src/Providers/index.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react"
+import { CartProvider } from "./Cart"
+
+interface CartProps {
+    children: ReactNode;
+  }
+
+export const Providers = ({children}: CartProps) =>{
+    
+    return(
+        <>
+
+            <CartProvider>
+               
+                    {children}
+                
+            </CartProvider>
+        </>
+    )
+}
